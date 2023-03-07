e84d951e81204148ddf3c1f72f02451e02e6439f
Ruben <rubenmontes330@gmail.com>
2023-03-07 18:37:32 +0000

    css concionado a las props del componente

--- src/App.js
+++ src/App.js
@@ -8,10 +8,22 @@ const Content= styled.div`
   padding: 20px 25px;
 
 `
+
+const Button = styled.button`
+  background-color: ${props=> props.primary? 'red' : 'white'};
+  color: ${props=> props.primary? 'white': 'red'};
+  padding 10px 15px;
+  border: solid 2px red;
+  border-radius: 4px;
+`
 function App() {
   return (
     <Content>
       <P>Hola</P>
+      <Button> Enviar</Button>
+      <br/>
+      <br/>
+      <Button primary> Enviar</Button>
     </Content>
   );
 }
