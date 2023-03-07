246aa09c3cc70b689ace601e48bc852de03b38c5
Ruben <rubenmontes330@gmail.com>
2023-03-07 19:24:53 +0000

    pseudo elementos y selectores

--- src/App.js
+++ src/App.js
@@ -10,11 +10,23 @@ const Content= styled.div`
 `
 
 const Button = styled.button`
+  transition: box-shadow 0.2s ease;
   background-color: ${props=> props.primary? 'red' : 'white'};
   color: ${props=> props.primary? 'white': 'red'};
   padding 10px 15px;
   border: solid 2px red;
   border-radius: 4px;
+
+  &:hover {
+    box-shadow: 1px 2px 5px rgb(0,0,0,0.3)
+  }
+
+  &.secondary {
+    background-color:blue;
+    border: solid 2px blue;
+    color:white
+
+  }
 `
 const BlockButton= styled(Button)`
   width:100%;
@@ -28,6 +40,7 @@ const Link = ({ className, ...props})=> {
   
 const StyledLink = styled(Link)` 
   color: blue 
+  
 `
 function App() {
   return (
@@ -37,6 +50,7 @@ function App() {
       <br/>
       <br/>
       <Button primary> Enviar</Button>
+      <Button  className='secondary'> Enviar</Button>
       <br/>
       <br/>
       <BlockButton> Enviar</BlockButton>
